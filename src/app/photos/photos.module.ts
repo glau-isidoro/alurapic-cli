ebdd912797a5125fc5445fee61a6ac192be9c896
Glaucia Isidoro <glaucia.isidoro@gtpautomation.com>
2018-07-12 16:33:02 -0300

    PhotoListCompoment

--- src/app/photos/photos.module.ts
+++ src/app/photos/photos.module.ts
@@ -1,11 +1,22 @@
-import { NgModule } from "@angular/core";
-import { PhotoComponent } from "./photo/photo.component";
-import { HttpClientModule } from "@angular/common/http/http";
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
+
+import { PhotoComponent } from './photo/photo.component';
+import { PhotoListComponent } from './photo-list/photo-list.component';
+import { PhotoService } from './photo/photo.service';
 
 
 @NgModule({
-    declarations: [ PhotoComponent ],
-    exports: [ PhotoComponent ],
-    imports: [ HttpClientModule ]
+    declarations: [ 
+        PhotoComponent,
+        PhotoListComponent
+    ],
+    exports: [ PhotoListComponent ],
+    imports: [ 
+        HttpClientModule,
+        CommonModule,
+    ],
+    providers: [ PhotoService ]
 })
 export class PhotosModule {}
