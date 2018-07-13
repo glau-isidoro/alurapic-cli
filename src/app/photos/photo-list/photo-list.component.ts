4ab5919c274b9178edde8c77f1f4e63c6a5150ec
Glaucia Isidoro <glaucia.isidoro@gtpautomation.com>
2018-07-13 15:48:08 -0300

    Search Component

--- src/app/photos/photo-list/photo-list.component.ts
+++ src/app/photos/photo-list/photo-list.component.ts
@@ -1,22 +1,19 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { Subject } from 'rxjs/Subject';
-import { debounceTime } from 'rxjs/operators';
 
 import { Photo } from '../photo/photo';
 import { PhotoService } from '../photo/photo.service';
-import { THROW_IF_NOT_FOUND } from '@angular/core/src/di/injector';
 
 @Component({
   selector: 'app-photo-list',
   templateUrl: './photo-list.component.html',
   styleUrls: ['./photo-list.component.css']
 })
-export class PhotoListComponent implements OnInit, OnDestroy {
+export class PhotoListComponent implements OnInit {
 
   photos: Photo[] = []
   filter: string = '';
-  debounce: Subject<string> = new Subject<string>();
+  
   hasMore: boolean = true;
   currentPage: number = 1;
   userName: string = '';
@@ -29,15 +26,6 @@ export class PhotoListComponent implements OnInit, OnDestroy {
   ngOnInit(): void {
     this.userName = this.activatedRoute.snapshot.params.userName;
     this.photos = this.activatedRoute.snapshot.data['photos'];
-    this.debounce
-      .pipe(debounceTime(300))
-      .subscribe(filter => this.filter = filter);
-  }
-
-  //para garantir que o debounce não vai ficar alocando memória
-  //quando o componente não estiver mais sendo usado.
-  ngOnDestroy(): void {
-    this.debounce.unsubscribe();
   }
 
   load() {
@@ -47,6 +35,7 @@ export class PhotoListComponent implements OnInit, OnDestroy {
         //this.photos.push(...newPhotos); não funciona pois o angular não detecta que o array mudou.
         //(...newPhotos) faz push de cada item do array newPhotos para o array this.photos
         this.photos = this.photos.concat(newPhotos); //assim o angular detecta a mudança
+        this.filter = '';
         if(!newPhotos.length) this.hasMore = false;
       });
   }
