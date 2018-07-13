3487fe5d9606ab8ec471bd9df6f6a9b279a27d41
Glaucia Isidoro <glaucia.isidoro@gtpautomation.com>
2018-07-13 11:42:27 -0300

    debounce for better search.

--- src/app/photos/photo-list/photo-list.component.ts
+++ src/app/photos/photo-list/photo-list.component.ts
@@ -1,21 +1,33 @@
-import { Component, OnInit } from '@angular/core';
-import { Photo } from '../photo/photo';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Subject } from 'rxjs/Subject';
+import { debounceTime } from 'rxjs/operators';
+
+import { Photo } from '../photo/photo';
 
 @Component({
   selector: 'app-photo-list',
   templateUrl: './photo-list.component.html',
   styleUrls: ['./photo-list.component.css']
 })
-export class PhotoListComponent implements OnInit {
+export class PhotoListComponent implements OnInit, OnDestroy {
 
   photos: Photo[] = []
   filter: string = '';
+  debounce: Subject<string> = new Subject<string>();
 
   constructor( private activatedRoute: ActivatedRoute ) {}
   
   ngOnInit(): void {
     this.photos = this.activatedRoute.snapshot.data['photos'];
+    this.debounce
+      .pipe(debounceTime(300))
+      .subscribe(filter => this.filter = filter);
   }
 
+  //para garantir que o debounce não vai ficar alocando memória
+  //quando o componente não estiver mais sendo usado.
+  ngOnDestroy(): void {
+    this.debounce.unsubscribe();
+  }
 }
