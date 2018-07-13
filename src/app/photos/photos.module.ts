d518a79be5a5b337f96cfa1427c077adbd443fd6
Glaucia Isidoro <glaucia.isidoro@gtpautomation.com>
2018-07-13 10:03:42 -0300

    Interface onchanges.

--- src/app/photos/photos.module.ts
+++ src/app/photos/photos.module.ts
@@ -6,13 +6,15 @@ import { PhotoComponent } from './photo/photo.component';
 import { PhotoListComponent } from './photo-list/photo-list.component';
 import { PhotoService } from './photo/photo.service';
 import { PhotoFormComponent } from './photo-form/photo-form.component';
+import { PhotosComponent } from './photo-list/photos/photos.component';
 
 
 @NgModule({
     declarations: [ 
         PhotoComponent,
         PhotoListComponent,
-        PhotoFormComponent
+        PhotoFormComponent,
+        PhotosComponent
     ],
     exports: [ PhotoListComponent ],
     imports: [ 
